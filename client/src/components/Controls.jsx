e2ddd721d4bfda0bdca74f3b69c438d9d81c3ada
AidenAstle <69543550+AidenAstle@users.noreply.github.com>
2025-04-29 17:59:23 -0400

    converted controls to mui checkboxes and grid for styling and ease

--- client/src/components/Controls.jsx
+++ client/src/components/Controls.jsx
@@ -1,13 +1,9 @@
-import { Box, Checkbox } from '@mui/material'
-
-const Controls = ({ 
-    toggles, 
-    onToggle 
-  }) => {
-    return (
-      <Box className='map-controls' component='div' sx={{display: 'flex', maxHeight: '50px', width: '80%', padding:'5px', margin:'10px', flexDirection : 'column', flexWrap:'wrap', justifyContent: 'space-around', alignContent:'stretch'}}>
-        {toggles.map(toggle => (
-          <label key={toggle.id}>
+import { Box, Checkbox, Grid} from '@mui/material'
+/*
+<Box className='map-controls' component='div' sx={{display: 'flex', maxHeight: '50px', width: '80%', padding:'5px', margin:'10px', 
+      flexDirection: 'column', flexWrap:'wrap', justifyContent: 'space-evenly'}}
+>
+<label key={toggle.id}>
             <input 
               type="checkbox"
               checked={toggle.checked}
@@ -15,8 +11,25 @@ const Controls = ({
             />
             {toggle.label}
           </label>
+</Box>
+*/
+const Controls = ({ 
+    toggles, 
+    onToggle 
+  }) => {
+    return (
+      <Grid container spacing={2} direction={"row"} justifyContent={"space-evenly"} alignItems={"center"} textAlign={'center'} sx={{maxWidth: '80%'}}>
+        {toggles.map(toggle => (
+          <Grid size={3}>
+            <label key={toggle.id}>
+              <Checkbox sx={{color:'white'}}
+                defaultChecked={toggle.checked} label={`${toggle.label}`} onChange={() => onToggle(toggle.id)}
+              />
+              {toggle.label}
+            </label>
+          </Grid>
         ))}
-      </Box>
+      </Grid>
     );
   };
   
